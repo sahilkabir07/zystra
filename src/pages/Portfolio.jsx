7bef366984b47ef80344e8d4322b986037e1247a
sahilkabir07 <sahilakthar8986@gmail.com>
2025-07-14 22:54:23 +0530

    portfolio file modifies

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import PortfolioBanner from '../components/portfolioComponents/portfolioBanner'
-
+import PortfolioBanner from '../components/portfolioComp/PortfolioBanner'
 
 const Portfolio = () => {
     return (
